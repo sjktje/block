7359a6421ade3a8c3a8a9fbb20238a5c0ead668b
Svante Kvarnstrom <sjk@ankeborg.nu>
2010-10-08 00:16:05 +0200

    Added missing newline char in --help text

--- block.c
+++ block.c
@@ -118,7 +118,7 @@ usage(char *p)
             "   -k, --kill-states           kill states\n"
             "   -n, --no-add                only kill states\n"
             "   -t, --table                 table to add IPs to\n"
-            "   -u, --unblock               removes IPs instead of adding them"
+            "   -u, --unblock               removes IPs instead of adding them\n"
             "   -v, --version               display program version\n\n"
             "See http://ankeborg.nu/wiki/Block for more information.\n");
     exit(1);
